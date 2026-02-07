7a764788830b2e38aae4b54e24d57f73ae3a6a13
HarleyQs <aispa702@gmail.com>
2026-02-07 23:27:50 +0700

    Revert SaleRepository to use BranchSale instead of Sale interface

--- bakery-backend/src/main/java/com/familybakery/bakery_backend/repository/SaleRepository.java
+++ bakery-backend/src/main/java/com/familybakery/bakery_backend/repository/SaleRepository.java
@@ -1,7 +1,7 @@
 package com.familybakery.bakery_backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.familybakery.bakery_backend.model.sale.Sale;
+import com.familybakery.bakery_backend.model.sale.BranchSale;
 
-public interface SaleRepository extends JpaRepository<Sale, Long> {
+public interface SaleRepository extends JpaRepository<BranchSale, Long> {
 }
